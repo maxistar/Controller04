c404b92a37b34ba4d0001341eafe269ec832978a
Maxim Starikov <mstarikov@elance-odesk.com>
2016-12-24 16:24:46 +0400

    commit the latest working version

--- dimmer_types.h
+++ dimmer_types.h
@@ -1,9 +1,5 @@
 // button debouncer, how many ms to debounce, 5+ ms is usually plenty
 #define DEBOUNCE 50  
 
-#define ANIMATION_STEP 10
-
-//how long to go into setup mode
-#define LONG_PRESS 5000  
 
 
